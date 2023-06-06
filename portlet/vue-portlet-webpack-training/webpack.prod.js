7b7423b507ce5562b995d06ba7f681e9e4d8e8f9
Romain Dénarié <romain.denarie@exoplatform.com>
2023-06-06 16:04:06 +0200

    Add empty project for training purpose

---
+++ portlet/vue-portlet-webpack-training/webpack.prod.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader')
+
+const config = {
+  mode: 'production',
+  context: path.resolve(__dirname, '.'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        use: [
+          'vue-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ESLintPlugin({
+      files: [
+        './src/main/webapp/vue-app/*.js',
+        './src/main/webapp/vue-app/*.vue',
+        './src/main/webapp/vue-app/**/*.js',
+        './src/main/webapp/vue-app/**/*.vue',
+      ],
+    }),
+    new VueLoaderPlugin()
+  ],
+  entry: {
+
+  },
+  output: {
+    path: path.join(__dirname, 'target/vue-webpack-sample/'),
+    filename: 'js/[name].bundle.js',
+    libraryTarget: 'amd'
+  },
+  externals: {
+    vue: 'Vue',
+    vuetify: 'Vuetify',
+    jquery: '$',
+  },
+};
+
+module.exports = config;
