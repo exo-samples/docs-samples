2cc4824cf60929c9f17b7beed1f27fe64bb3ff3c
Romain Dénarié <romain.denarie@exoplatform.com>
2025-11-21 15:20:36 +0100

    Update to eXo 7.1.x

--- portlet/vue-portlet-webpack/src/main/webapp/vue-app/components/app.vue
+++ portlet/vue-portlet-webpack/src/main/webapp/vue-app/components/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="vue_webpack_sample">
+  <div id="vue_webpack_sample" class="v-application">
     <span>{{ $t('sample.i18n.label') }}</span>
     <todo-list />
   </div>
--- portlet/vue-portlet-webpack/src/main/webapp/vue-app/main.js
+++ portlet/vue-portlet-webpack/src/main/webapp/vue-app/main.js
@@ -10,12 +10,14 @@ const resourceBundleName = 'locale.addon.Sample';
 // Get the URL to load the resource bundles
 const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/${resourceBundleName}-${lang}.json`;
 
-// getting the resource bundles
-exoi18n.loadLanguageAsync(lang, url)
-  .then(i18n => {
-    // init Vue app when locale resources are ready
-    new Vue({
-      render: h => h(app),
-      i18n
-    }).$mount('#vue_webpack_sample'); // mount the application on the HTML element with id = 'vue_webpack_sample'
-  });
+export function init() {
+  // init Vue app when locale resources are ready
+  exoi18n.loadLanguageAsync(lang, url)
+    .then(i18n => {
+      // init Vue app when locale resources are ready
+      new Vue({
+        render: h => h(app),
+        i18n
+      }).$mount('#vue_webpack_sample'); // mount the application on the HTML element with id = 'vue_webpack_sample'
+    });
+}
